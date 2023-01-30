eb713cabda0a551ff83c44e35dff409062b71aca
Jeff Sandberg <paradox460@gmail.com>
2023-01-30 00:37:39 -0700

    Reformat slug, add footer

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,42 +1,60 @@
 <template>
-  <ContentDoc >
+  <ContentDoc>
     <template #default="{ doc }">
       <TableOfContents :activeToc="activeToc" :doc="doc" />
-      <ContentRenderer ref="nuxtContent" :value="doc" id="content" class="post"/>
+      <div id="content" class="post">
+        <ContentRenderer ref="nuxtContent" :value="doc" />
+        <footer class="articlefooter">
+          The article "{{ doc.title }}" was written on
+          <Timestamp :datetime="doc.date" />.
+          <a :href="`https://github.com/paradox460/pdx.su/blob/main/content/${doc._file}`" class="ghl"
+            target="_blank">View Source</a>
+        </footer>
+      </div>
     </template>
   </ContentDoc>
 </template>
 
+<style lang="stylus" scoped>
+.articlefooter
+  margin-top 3rem
+  text-align center
+  border: 1px solid var(--base01)
+  border-radius: 5px
+  padding: 0.5rem
+  color: var(--base05)
+</style>
+
 <script setup lang="ts">
-  import { Ref } from "vue"
+import { Ref } from "vue"
 
-  const activeToc = ref()
-  const nuxtContext = ref()
+const activeToc = ref()
+const nuxtContext = ref()
 
-  const observer: Ref<IntersectionObserver | null | undefined> = ref()
-  const observerOptions = reactive({
-    root: nuxtContext.value,
-    threshold: 1
-  })
+const observer: Ref<IntersectionObserver | null | undefined> = ref()
+const observerOptions = reactive({
+  root: nuxtContext.value,
+  threshold: 1
+})
 
-  onMounted(() => {
-    observer.value = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (window.scrollY <= 0) {
-          activeToc.value = null;
-        }
-        else if (entry.isIntersecting) {
-          activeToc.value = entry.target.getAttribute("id")
-        }
-      })
-    }, observerOptions)
-
-    document.querySelectorAll('#content :is(h2, h3)[id]').forEach((el) => {
-      observer.value?.observe((el))
+onMounted(() => {
+  observer.value = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (window.scrollY <= 0) {
+        activeToc.value = null;
+      }
+      else if (entry.isIntersecting) {
+        activeToc.value = entry.target.getAttribute("id")
+      }
     })
-  })
+  }, observerOptions)
 
-  onUnmounted(() => {
-    observer.value?.disconnect()
+  document.querySelectorAll('#content :is(h2, h3)[id]').forEach((el) => {
+    observer.value?.observe((el))
   })
+})
+
+onUnmounted(() => {
+  observer.value?.disconnect()
+})
 </script>
