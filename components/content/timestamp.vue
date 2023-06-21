989e150439a3babe1ff1246dbef671c009fa9e3b
Jeff Sandberg <paradox460@gmail.com>
2023-06-21 02:39:08 -0600

    Update timestamp component to reflect local format

--- components/content/timestamp.vue
+++ components/content/timestamp.vue
@@ -3,17 +3,19 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
 const props = defineProps<{ datetime?: string }>();
 
-let language = "us"
+let language = ref("en-US")
 
 onMounted(() => {
-  language = navigator.language;
+  language.value = navigator.language;
 })
 
 const niceTimeStamp = computed(() => {
   if (props.datetime) {
-    return (new Intl.DateTimeFormat(language, { dateStyle: "short", timeStyle: undefined }).format(Date.parse(props.datetime)));
+    return (new Intl.DateTimeFormat(language.value, { dateStyle: "short", timeStyle: undefined }).format(Date.parse(props.datetime)));
   }
 })
 
