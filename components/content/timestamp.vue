3875e94471bf5dd4c71b35ebbeeef3a21ace302c
Jeff Sandberg <paradox460@gmail.com>
2023-07-05 23:05:20 -0600

    Add timestamps to timestamp component
    Kind of redundant, but to this date it only displayed the Date, not the time

--- components/content/timestamp.vue
+++ components/content/timestamp.vue
@@ -1,11 +1,11 @@
 <template>
-  <time v-if="!!datetime" :datetime="datetime">{{ niceTimeStamp }}</time>
+  <time v-if="!!datetime" :datetime="datetime" :title="fullTimeStamp">{{ niceTimeStamp }}</time>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const props = defineProps<{ datetime?: string }>();
+const props = defineProps<{ datetime?: string, timestamp?: boolean, hovertimestamp?: boolean }>();
 
 let language = ref("en-US")
 
@@ -15,7 +15,13 @@ onMounted(() => {
 
 const niceTimeStamp = computed(() => {
   if (props.datetime) {
-    return (new Intl.DateTimeFormat(language.value, { dateStyle: "short", timeStyle: undefined }).format(Date.parse(props.datetime)));
+    return (new Intl.DateTimeFormat(language.value, { dateStyle: "short", timeStyle: props.timestamp ? "short" : undefined }).format(Date.parse(props.datetime)));
+  }
+})
+
+const fullTimeStamp = computed(() => {
+  if (props.datetime) {
+    return (new Intl.DateTimeFormat(language.value, { dateStyle: "full", timeStyle: props.hovertimestamp || props.timestamp ? "full" : undefined }).format(Date.parse(props.datetime)));
   }
 })
 
