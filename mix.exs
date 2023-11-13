9ffb56e36329115da831b56a2e3749e4a46c436e
Jeff Sandberg <paradox460@gmail.com>
2023-11-12 18:09:33 -0700

    Build MDex locally, for theme support

--- mix.exs
+++ mix.exs
@@ -22,7 +22,9 @@ defmodule Pdx.MixProject do
   defp deps do
     [
       {:tableau, "~> 0.11.1"},
-      {:temple, "~> 0.12"}
+      {:temple, "~> 0.12"},
+      {:rustler, ">= 0.0.0", optional: true},
+      {:mdex, path: "../mdex", override: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
