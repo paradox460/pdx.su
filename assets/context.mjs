49db4e45d4da49bacc81fbd4c9e6f315d4ef621a
Jeff Sandberg <paradox460@gmail.com>
2023-11-12 22:33:22 -0700

    Clean up build scripts for minification and mangle

--- assets/context.mjs
+++ assets/context.mjs
@@ -5,11 +5,19 @@ import autoprefixer from 'autoprefixer';
 import postcssPresetEnv from 'postcss-preset-env';
 import { minifyHTMLLiteralsPlugin } from 'esbuild-plugin-minify-html-literals';
 
+const dev = process.env.NODE_ENV !== "production";
+
 
 export default await esbuild.context({
   entryPoints: ["./css/style.scss", "./js/index.ts"],
   bundle: true,
-  sourcemap: true,
+  minify: !dev,
+  sourcemap: dev,
+  legalComments: "linked",
+  footer: {
+    js: "/*! Copyright 2023 Jeff Sandberg */",
+    css: "/* Copyright 2023 Jeff Sandberg */"
+  },
   outdir: "../_site",
   plugins: [minifyHTMLLiteralsPlugin(), sassPlugin({
     async transform(source, resolveDir) {
