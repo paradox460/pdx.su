0bea3db64f2df90fc8589a3a7ac332dad8a618c1
Jeff Sandberg <paradox460@gmail.com>
2023-11-21 16:15:21 -0700

    MDex got tomorrow night natively
    https://github.com/leandrocp/autumn/pull/12 merged into Autumn added
    tomorrow night theme
    
    https://github.com/leandrocp/mdex/commit/ffefcddf63710d66d926244da3e43d190986c48c
    added theme to MDex, was released in v0.1.13

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Pdx.MixProject do
       {:floki, "~> 0.35.2"},
       {:tableau, "~> 0.12.0"},
       {:temple, "~> 0.12"},
-      {:mdex, github: "paradox460/mdex", override: true},
+      {:mdex, "~> 0.1.13", override: true},
       {:rustler, ">= 0.0.0", optional: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
